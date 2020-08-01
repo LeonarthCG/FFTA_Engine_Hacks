752e72b4ac13605ea9f0c779e9cc94886b0dba35
LeonarthCG <leonarth.cg@gmail.com>
2020-08-01 14:00:43 +0200

    Fix JP display in battle
    JP display used to draw a red 0 when JP was at 0, which didn't really seem right to me, so I changed that.
    
    More importantly, since the highest digit was drawn individually and the rest handled by a different routine, things like "1000" would end up coming out as "1  0", this fixes the issue by drawing all digits using a new routine.

--- Engine Hacks/jpLearn/jpmax2.s
+++ Engine Hacks/jpLearn/jpmax2.s
@@ -1,32 +1,79 @@
 .thumb
 ldrh	r1,[r0]		@jp
+mov	r0,r7
+ldr	r2,tiles
+push	{r4-r7}
+mov	r4,r1		@jp
+mov	r5,r0		@tilemap buffer
+ldr	r6,=#0xD01E	@base tile
+mov	r7,r4		@remainder after division
+lsl	r2,#0x10
+lsr	r2,#0x0F
+add	r5,r2
+
 ldr	r0,=#9999
-cmp	r1,r0
-blo	noProblem1
-mov	r1,r0
-noProblem1:
+cmp	r4,r0
+blo	not5digits
+mov	r4,r0
+not5digits:
 
+@draw the fourth digit
 ldr	r0,=#1000
-cmp	r1,r0
-blo	noProblem2
-mov	r2,r1
-mov	r1,r0
-mov	r0,r2
+cmp	r4,r0
+blo	not4digits
+mov	r0,r4
+ldr	r1,=#1000
 swi	#6
-mov	r3,r0
-mov	r0,r7
-add	r0,#0x9A
-ldr	r2,=0xD01E
-add	r2,r3
-strh	r2,[r0]
-noProblem2:
+mov	r7,r1
+add	r0,r6
+strh	r0,[r5,#0x00]
+not4digits:
 
-@r1 = JP
+@draw the third digit
+ldr	r0,=#100
+cmp	r4,r0
+blo	not3digits
 mov	r0,r7
-mov	r2,#0x4E
+ldr	r1,=#100
+swi	#6
+mov	r7,r1
+add	r0,r6
+strh	r0,[r5,#0x02]
+not3digits:
 
-ldr	r3,=#0x802CD54
+@draw the second digit
+ldr	r0,=#10
+cmp	r4,r0
+blo	not2digits
+mov	r0,r7
+ldr	r1,=#10
+swi	#6
+mov	r7,r1
+add	r0,r6
+strh	r0,[r5,#0x04]
+not2digits:
+
+@draw the last 0
+strh	r6,[r5,#0x06]
+
+@draw the first digit
+ldr	r0,=#1
+cmp	r4,r0
+blo	not1digits
+mov	r0,r7
+ldr	r1,=#1
+swi	#6
+mov	r7,r1
+add	r0,r6
+strh	r0,[r5,#0x06]
+add	r5,#2
+not1digits:
+
+end:
+pop	{r4-r7}
+ldr	r3,=#0x802CD58
 mov	lr,r3
-
-mov	r3,#0x1
 .short	0xF800
+.align
+.ltorg
+tiles:
